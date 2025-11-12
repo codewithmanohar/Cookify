b352edab3770256a0e37e173378a891a73a36477
codewithmanohar <mksahni2555@gmail.com>
2025-11-12 23:29:11 +0530

    Zustand Installed

---
+++ Store/useIngredientsStore.jsx
@@ -0,0 +1,12 @@
+// stores/useCounterStore.js
+import { create } from 'zustand'
+
+const useCounterStore = create((set) => ({
+  count: 0,
+
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 })),
+  reset: () => set({ count: 0 }),
+}));
+
+export default useCounterStore
--- app/ingredients/page.js
+++ app/ingredients/page.js
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client"; 
 
-const page = () => {
+import useCounterStore from "@/Store/useIngredientsStore";
+
+export default function Counter() {
+  const { count, increment, decrement, reset } = useCounterStore();
+
   return (
-    <div>page</div>
-  )
+    <div className="flex flex-col items-center gap-3 mt-10">
+      <h1 className="text-2xl font-bold">Counter: {count}</h1>
+      <div className="flex gap-4">
+        <button onClick={increment} className="px-4 py-2 bg-green-500 text-white rounded">
+          +
+        </button>
+        <button onClick={decrement} className="px-4 py-2 bg-red-500 text-white rounded">
+          -
+        </button>
+        <button onClick={reset} className="px-4 py-2 bg-gray-500 text-white rounded">
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
-
-export default page
