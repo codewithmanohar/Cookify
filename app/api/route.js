a32f77709302726bae79ee263464271b7811d0a0
codewithmanohar <mksahni2555@gmail.com>
2025-11-18 23:20:59 +0530

    commit : setup gemini route for imgae generation

---
+++ app/api/route.js
@@ -0,0 +1,12 @@
+export async function GET() {
+  return Response.json({ message: "This is a GET request" });
+}
+
+export async function POST(request) {
+  const body = await request.json();
+
+  return Response.json({
+    message: "This is a POST request",
+    receivedData: body
+  });
+}
--- app/page.js
+++ app/page.js
@@ -3,6 +3,16 @@ import { Header } from "@/components/Header";
 import { HeroSection } from "@/components/HeroSection";
 
 export default function Home() {
+
+// async function fetchData() {
+//   const res = await fetch("http://localhost:3000/api/Auth");
+//   const data = await res.json();
+//   console.log(data);
+// }
+
+// fetchData();
+
+
   return (
     <div className="container mx-auto my-14 relative">
       <section className="w-full">
