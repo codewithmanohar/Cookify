dad67e343b6548c222b7d7992fd14e546f1940b8
codewithmanohar <mksahni2555@gmail.com>
2025-12-02 23:16:48 +0530

    commit : updated theme using tweakcn and about page

--- app/about/page.js
+++ app/about/page.js
@@ -8,75 +8,85 @@ import Link from "next/link"
 
 
 const page = () => {
-  return (
-    <>
-     <div className="container mx-auto">
-        {/* Hero section */}
-        <div 
-            className={`min-h-[350px] flex flex-col items-center justify-center text-center px-4 bg-[url(../public/hero_about_img.png)] opacity-85 max-w-4xl rounded-2xl mx-auto mt-32 mb-10 `}
-        >
-                <h1 className='text-secondary text-5xl font-semibold py-5 '>
-                    Your Favorite Flavors 🍛
-                </h1>
-                <p className=' text-secondary text-sm text-wrap max-w-2xl  '>
-                    Rediscover your culinary journey with your hand-picked collection of delightful recipes.
-                </p>
-        </div>
+    return (
+        <>
+            <div className="container mx-auto">
+                {/* Hero section */}
+                <div
+                    className={`min-h-[350px] flex flex-col items-center justify-center text-center px-4 bg-[url(../public/hero_about_img.png)] opacity-85 max-w-4xl rounded-2xl mx-auto mt-32 mb-10 `}
+                >
+                    <h1 className='text-secondary text-5xl font-semibold py-5 '>
+                        Your Favorite Flavors 🍛
+                    </h1>
+                    <p className=' text-secondary text-sm text-wrap max-w-2xl  '>
+                        Rediscover your culinary journey with your hand-picked collection of delightful recipes.
+                    </p>
+                </div>
 
-        {/* features */}
-        <section className="flex items-center justify-center gap-10 flex-col my-10">
-            <h1 className="text-2xl font-bold">How CookMateAI Works</h1>
-            <div className="max-w-7xl flex items-center justify-center flex-wrap gap-10">
-                {features.map((item , index) => (
-                    <div
-                        className="max-w-xs bg-gray-50 rounded-2xl px-5 py-10 flex flex-col items-center"
-                        key={index}>
-                        <span  className="text-primary">{item.icon}</span>
-                        <h1 className="text-xl font-bold py-2">{item.title}</h1>
-                        <h2 className="text-sm text-zinc-500 max-w-56 text-center ">{item.description}</h2>
-                        <Image 
-                            className="object-fill py-5"
-                            src={item.image}
-                            width={250} 
-                            height={50} 
-                            alt="features_img"
-                        />
+                {/* features */}
+                <section className="flex items-center justify-center gap-10 flex-col my-10">
+                    <h1 className="text-2xl font-bold">How CookMateAI Works</h1>
+                    <div className="max-w-7xl flex items-center justify-center flex-wrap gap-10">
+                        {features.map((item, index) => (
+                            <div
+                                className="max-w-xs bg-gray-50 rounded-2xl px-5 py-10 flex flex-col items-center"
+                                key={index}>
+                                <span className="text-primary">{item.icon}</span>
+                                <h1 className="text-xl font-bold py-2">{item.title}</h1>
+                                <h2 className="text-sm text-zinc-500 max-w-56 text-center ">{item.description}</h2>
+                                <Image
+                                    className="object-fill py-5"
+                                    src={item.image}
+                                    width={250}
+                                    height={50}
+                                    alt="features_img"
+                                />
+                            </div>
+                        ))}
+
                     </div>
-                ))}
+                </section>
 
+                {/* Our Mission */}
+                <div
+                    className="min-h-[350px] flex flex-col items-center justify-center text-center 
+                                px-4 max-w-4xl rounded-2xl mx-auto mt-32 mb-10
+                                bg-cover bg-center bg-no-repeat opacity-90"
+                    style={{ backgroundImage: "url('/about_bg_img2.png')" }}
+                >
+                    <h1 className="text-white text-5xl font-semibold py-5">
+                        Our Culinary Mission
+                    </h1>
+
+                    <p className="text-gray-300 text-sm max-w-2xl">
+                        At CookMateAI, we believe that cooking should be an adventure. Our mission is to
+                        seamlessly blend artificial intelligence with culinary creativity, empowering
+                        home cooks to explore new flavors, reduce food waste, and discover the joy of
+                        personalized recipes tailored to their unique tastes and available ingredients.
+                        We're here to spark your next kitchen masterpiece.
+                    </p>
+                </div>
+
+
+                {/* Action Button */}
+                <div
+                    className="min-h-[250px] flex flex-col items-center justify-center text-center px-4  max-w-4xl rounded-2xl mx-auto mt-32 mb-10 bg-cover"
+                    style={{ backgroundImage: "url('/about_bg_img2.png')" }}
+                >
+                    <h1 className=' text-5xl font-semibold py-10 text-gray-200 '>
+                        Ready to Start Cooking?
+                    </h1>
+                    <Link href="/" >
+                        <Button>Generate Your Next Recipe <Sparkles /></Button>
+                    </Link>
+                </div>
+
+                <section>
+                    <Footer />
+                </section>
             </div>
-        </section>
-
-        {/* Our Mission */}
-        <div 
-            className="min-h-[350px] flex flex-col items-center justify-center text-center px-4 bg-gray-200 max-w-4xl rounded-2xl mx-auto mt-32 mb-10"
-        >
-                <h1 className='text-primary text-5xl font-semibold py-5 '>
-                    Our Culinary Mission
-                </h1>
-                <p className=' text-gray-500 text-sm text-wrap max-w-2xl  '>
-                    At CookMateAI, we believe that cooking should be an adventure. Our mission is to seamlessly blend artificial intelligence with culinary creativity, empowering home cooks to explore new flavors, reduce food waste, and discover the joy of personalized recipes tailored to their unique tastes and available ingredients. We're here to spark your next kitchen masterpiece.
-                </p>
-        </div>
-
-        {/* Action Button */}
-        <div 
-            className="min-h-[250px] flex flex-col items-center justify-center text-center px-4 bg-gray-200 max-w-4xl rounded-2xl mx-auto mt-32 mb-10"
-        >
-                <h1 className=' text-5xl font-semibold py-10 '>
-                    Ready to Start Cooking?
-                </h1>
-                <Link href="/" >
-                    <Button>Generate Your Next Recipe <Sparkles /></Button>
-                </Link>
-        </div>
-
-        <section>
-            <Footer />
-        </section>
-     </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default page
